88a4c9a9753761cfd81379ab841450e67a972b79
michamettler <mettlermicha@gmail.com>
2026-03-16 23:20:36 +0100

    cleanup code

--- scripts/transcribe/run_transcription.py
+++ scripts/transcribe/run_transcription.py
@@ -2,4 +2,4 @@ from transcribe_whisper import transcribe
 
 if __name__ == "__main__":
     transcribe("small", "DIT", "dialect-ignorant-transcript.tsv", "zurich_subset")
-    #transcribe("large", "DAT", "dialect-aware-transcript.tsv", "zurich_subset")
+    transcribe("large", "DAT", "dialect-aware-transcript.tsv", "zurich_subset")
